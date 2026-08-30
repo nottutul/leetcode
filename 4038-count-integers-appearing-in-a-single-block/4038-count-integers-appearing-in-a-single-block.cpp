447467f2d8b8ff116da2992ee6abbbf87bfdbce5
Tutul Kumar Biswas <164195132+nottutul@users.noreply.github.com>
2026-08-30 17:32:35 +0600

    Time: 0 ms (100%), Space: 21.6 MB (6.35%) - LeetHub

--- 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
         unordered_map<int, vector<int>> mp;
-        int count=0;
+        int count = 0;
 
-        for(int i=0; i<nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]].push_back(i);
         }
 
-        for(auto e: mp){
-            if(e.second.size() == 1){
-                count++;
-            }
-            else{
-                int ck=0;
-                for(int i=0; i<e.second.size()-1; i++){
-                    if(e.second[i+1] - e.second[i] == 1) ck=1;
-                    else{
-                        ck=0;
-                        break;
-                    } 
+        for (auto e : mp) {
+            bool special = true;
+
+            for (int i = 0; i < e.second.size() - 1; i++) {
+                if (e.second[i + 1] - e.second[i] != 1) {
+                    special = false;
+                    break;
                 }
-                if(ck == 1) count++;
             }
 
+            if (special)
+                count++;
         }
+
         return count;
     }
 };
